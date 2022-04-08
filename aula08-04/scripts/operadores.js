889b6d3cce3a9a7869fa796b5da7e9012da31e76
Thyara Nonato <thyaranonato@outlook.com>
2022-04-08 14:09:48 -0300

    add operadores js

---
+++ aula08-04/scripts/operadores.js
@@ -0,0 +1,12 @@
+let numero = 10;
+
+console.log(numero == "10"); // testando somente valor
+console.log(numero === "10"); // testando valor e tipo
+console.log(typeof numero);
+
+console.log(numero != "10"); // testando somente valor
+console.log(numero !== "10"); // testando valor e tipo
+
+if (numero == "10") {
+    console.log("ola");
+}
