dc42628497744e412fbf705a910334cb204c337a
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 13:15:55 -0300

    condicionais ts

---
+++ aula19-04-TypeScript/condicionais.ts
@@ -0,0 +1,9 @@
+var x = 30, y = 30;
+
+if(x < y) {
+    console.log(`${x} é menor do que ${y}`);
+} else if(x > y) {
+    console.log(`${x} é maior do que ${y}`);
+} else {
+    console.log(`${x} é igual a ${y}`);
+}
