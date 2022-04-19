c91563ce4d907a33835efe2854cb27b1d56fc392
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 13:46:18 -0300

    funçoes ts

--- aula19-04-TypeScript/funcoes.ts
+++ aula19-04-TypeScript/funcoes.ts
@@ -1,3 +1,7 @@
 var nome: string;
 nome = "Ana";
 console.log(nome);
+
+function soma(n1:number, n2:number):number {
+    return n1 + n2;
+}
--- aula19-04-TypeScript/scripts/funcoes.js
+++ aula19-04-TypeScript/scripts/funcoes.js
@@ -2,3 +2,6 @@
 var nome;
 nome = "Ana";
 console.log(nome);
+function soma(n1, n2) {
+    return n1 + n2;
+}
