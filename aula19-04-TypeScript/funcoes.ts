1d1545452e403f64a236036fcf8a8a8efe76a27b
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 14:58:02 -0300

    validação funções ts-html

--- aula19-04-TypeScript/funcoes.ts
+++ aula19-04-TypeScript/funcoes.ts
@@ -2,6 +2,51 @@ var nome: string;
 nome = "Ana";
 console.log(nome);
 
-function soma(n1:number, n2:number):number {
-    return n1 + n2;
+function soma(n1:number, n2:number):number | unknown {
+    try {
+        if(n1 ===  0 || n2 === 0) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 + n2;
+   } catch(error) {
+        return error;
+   }
+}
+
+function subtracao(n1:number, n2:number):number | unknown {
+    try {
+        if(n1 ===  0 || n2 === 0) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 - n2;
+   } catch(error) {
+        return error;
+   }
+}
+
+function multiplicacao(n1:number, n2:number):number | unknown {
+    try {
+        if(n1 ===  0 || n2 === 0) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 * n2;
+   } catch(error) {
+        return error;
+   }
+}
+
+function divisao(n1:number, n2:number):number | unknown {
+    try {
+        if(n1 === 0 || n2 === 0) {
+            throw "Informe um valor para n1 e n2.";
+        }
+
+        if(n2 === 0) {
+            throw "Não é possível dividir por zero.";
+        }
+
+        return n1 / n2;
+   } catch(error) {
+        return error;
+   }
 }
--- aula19-04-TypeScript/scripts/funcoes.js
+++ aula19-04-TypeScript/scripts/funcoes.js
@@ -3,5 +3,49 @@ var nome;
 nome = "Ana";
 console.log(nome);
 function soma(n1, n2) {
-    return n1 + n2;
+    try {
+        if (n1 === 0 || n2 === 0) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 + n2;
+    }
+    catch (error) {
+        return error;
+    }
+}
+function subtracao(n1, n2) {
+    try {
+        if (n1 === undefined || n2 === undefined) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 - n2;
+    }
+    catch (error) {
+        return error;
+    }
+}
+function multiplicacao(n1, n2) {
+    try {
+        if (n1 === undefined || n2 === undefined) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        return n1 * n2;
+    }
+    catch (error) {
+        return error;
+    }
+}
+function divisao(n1, n2) {
+    try {
+        if (n1 === undefined || n2 === undefined) {
+            throw "Informe um valor para n1 e n2.";
+        }
+        if (n2 === 0) {
+            throw "Não é possível dividir por zero.";
+        }
+        return n1 / n2;
+    }
+    catch (error) {
+        return error;
+    }
 }
