b4a0a6b6bb764f45956f2b95f9d0330f1d703e01
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 11:30:43 -0300

    tuple ts

---
+++ aula19-04-TypeScript/scripts/tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+var boletim;
+boletim = ["Maria", 10, 75];
+console.log(boletim);
+var estoque;
+estoque = ["Feijão", 150];
+console.log(estoque);
---
+++ aula19-04-TypeScript/tuple.ts
@@ -0,0 +1,7 @@
+var boletim:[string, number, number];
+boletim = ["Maria", 10, 75];
+console.log(boletim);
+
+var estoque:[string, number];
+estoque = ["Feijão", 150];
+console.log(estoque);
