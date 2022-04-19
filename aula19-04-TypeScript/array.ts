f94310a693c1e5e532fadd33773734134e68f920
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 11:02:05 -0300

    array ts

---
+++ aula19-04-TypeScript/array.ts
@@ -0,0 +1,8 @@
+var frutas: string[] = ["maça", "banana", "uva"];
+console.log(frutas);
+
+frutas.push("melancia");
+console.log(frutas);
+
+var numerosSorteados:number[] = [10, 40, 57, 2];
+console.log(numerosSorteados);
---
+++ aula19-04-TypeScript/scripts/array.js
@@ -0,0 +1,7 @@
+"use strict";
+var frutas = ["maça", "banana", "uva"];
+console.log(frutas);
+frutas.push("melancia");
+console.log(frutas);
+var numerosSorteados = [10, 40, 57, 2];
+console.log(numerosSorteados);
