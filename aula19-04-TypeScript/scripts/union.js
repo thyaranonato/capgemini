89d56bae8764a188f927ac27e4ac8e820c598965
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 11:12:30 -0300

    union ts

--- aula19-04-TypeScript/scripts/union.js
+++ aula19-04-TypeScript/scripts/union.js
@@ -1,16 +1,13 @@
-var cpf:(string | number);
+"use strict";
+var cpf;
 cpf = "123.456.789-10";
 console.log(cpf);
-
 cpf = 12345645610;
 console.log(cpf);
-
-var sexo:(string | number | boolean);
+var sexo;
 sexo = "masculino";
 console.log(sexo);
-
 sexo = 1;
 console.log(sexo);
-
 sexo = false;
 console.log(sexo);
