faa549f22056db09fa62c26c3e41084bf22ace57
Thyara Nonato <thyaranonato@outlook.com>
2022-04-19 11:42:21 -0300

    array de tuple

--- aula19-04-TypeScript/scripts/tuple.js
+++ aula19-04-TypeScript/scripts/tuple.js
@@ -5,3 +5,8 @@ console.log(boletim);
 var estoque;
 estoque = ["Feijão", 150];
 console.log(estoque);
+var participantes = [];
+participantes.push(["José", 26]);
+participantes.push(["Maria", 18]);
+participantes.push(["Pedro", 16]);
+console.log(participantes);
--- aula19-04-TypeScript/tuple.ts
+++ aula19-04-TypeScript/tuple.ts
@@ -5,3 +5,10 @@ console.log(boletim);
 var estoque:[string, number];
 estoque = ["Feijão", 150];
 console.log(estoque);
+
+// array de tipo tuple
+var participantes:[string, number][] = [];
+participantes.push(["José", 26]);
+participantes.push(["Maria", 18]);
+participantes.push(["Pedro", 16]);
+console.log(participantes);
