9809e0cdafeb0a47e0bdff6afe7bd5d43d5aea3d
Thyara Nonato <thyaranonato@outlook.com>
2022-04-25 16:35:33 -0300

    add cmponente consulta-contato

---
+++ aula25-04/aula-tarde/app-agenda/src/app/consulta-contato/consulta-contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-contato',
+  templateUrl: './consulta-contato.component.html',
+  styleUrls: ['./consulta-contato.component.css']
+})
+export class ConsultaContatoComponent {
+
+  constructor() { }
+
+}
