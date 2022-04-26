b8f84cc6a4899b69f7e7c3eaea1fdd783c60154f
Thyara Nonato <thyaranonato@outlook.com>
2022-04-26 07:54:05 -0300

    atera nome da pasta principal

--- aula25-04/aula-tarde/app-agenda/src/app/consulta-contato/consulta-contato.component.ts
+++ aula25-04/aula-tarde/app-agenda/src/app/consulta-contato/consulta-contato.component.ts
@@ -9,4 +9,23 @@ export class ConsultaContatoComponent {
 
   constructor() { }
 
+  load() {
+    fetch("http://localhost:3000/contatos")
+        .then(x => x.text())
+        .then(dados => this.display(dados));
+  }
+
+  display(lista: string) {
+    let dados = JSON.parse(lista);
+    let trs = "";
+    for(let obj in dados) {
+        trs += "<tr>"
+        trs += "<td>" + dados[obj].nome + "</td>"
+        trs += "<td>" + dados[obj].fone + "</td>"
+        trs += "<td>" + dados[obj].email + "</td>"
+        trs += "</tr>"
+    }
+    document.getElementsByTagName("tbody")[0].innerHTML = trs;
+  }
+
 }
