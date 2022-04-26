2daf25b20b7772e38ccaa6eca7c483a6caabca9b
Thyara Nonato <thyaranonato@outlook.com>
2022-04-26 11:40:18 -0300

    add getAll no consulta

--- aula25-04/aula-tarde/app-agenda/src/app/consulta-contato/consulta-contato.component.ts
+++ aula25-04/aula-tarde/app-agenda/src/app/consulta-contato/consulta-contato.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceContatoService } from '../service-contato.service';
 
 @Component({
   selector: 'app-consulta-contato',
@@ -6,31 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./consulta-contato.component.css']
 })
 export class ConsultaContatoComponent {
-  contatos = [{
-    id: 1,
-    nome: "Ana",
-    email: "ana@gmail.com",
-    fone: "123456789"
-  },
-  {
-    id: 2,
-    nome: "Pedro",
-    email: "pedro@gmail.com",
-    fone: "123456789"
-  },
-  {
-    id: 3,
-    nome: "João",
-    email: "joão@gmail.com",
-    fone: "123456789"
-  },
-  {
-    id: 4,
-    nome: "Paula",
-    email: "paula@gmail.com",
-    fone: "123456789"
-  }
-]
-  constructor() { }
+  contatos: any;
+  
+  constructor(private serviceContato: ServiceContatoService) {
+    this.serviceContato.getAll().subscribe(x => this.contatos = x);
+   }
+
+
 
 }
