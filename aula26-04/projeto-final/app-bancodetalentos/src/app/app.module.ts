89d97f5500380aee87691207df2b2ae69401ee18
Thyara Nonato <thyaranonato@outlook.com>
2022-04-26 14:29:09 -0300

    add página principal

--- aula26-04/projeto-final/app-bancodetalentos/src/app/app.module.ts
+++ aula26-04/projeto-final/app-bancodetalentos/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    CadastroComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
