3f75dc4903f153f18be895d94e5cc91fe2913704
Thyara Nonato <thyaranonato@outlook.com>
2022-04-27 08:30:10 -0300

    add componentes do projeto

--- aula27-04/app-loja/src/app/app.module.ts
+++ aula27-04/app-loja/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroProdutoComponent,
+    CarrinhoComponent,
+    PagamentoComponent,
+    CadastroUsuarioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
