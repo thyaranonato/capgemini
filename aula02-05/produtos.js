040e6909beb9b5059b589fc1bc1baab1ae550e3c
Thyara Nonato <thyaranonato@outlook.com>
2022-05-02 16:33:33 -0300

    implementa rotas para produtos

--- aula02-05/produtos.js
+++ aula02-05/produtos.js
@@ -12,6 +12,56 @@ app.get('/produtos', (req, res) => {
     res.status(200).send(dados);
 });
 
+app.get('/produtos/:idProduto', (req, res) => {
+    let idProduto = req.params.idProduto;
+    let produto = {};
+
+    for(let i of dados) {
+        if (i.id == idProduto) {
+            produto = i;
+        }
+    }
+    res.status(200).send(produto);
+});
+
+app.post('/produtos', (req, res) => {
+    let obj = {
+        id: dados.length + 1,
+        descricao: req.body.descricao,
+        preco: req.body.preco,
+        estoque: req.body.estoque
+    }
+    dados.push(obj);
+    res.status(201).send(obj);
+ });
+
+app.put('/produtos/:idProduto', (req, res) => {
+    let idProduto = req.params.idProduto;
+    let update = {};
+    for(let i of dados) {
+        if(i.id == idProduto) {
+            i.descricao = req.body.descricao,
+            i.preco = req.body.preco,
+            i.estoque = req.body.estoque
+            update = i;
+        }
+    }
+    res.status(200).send(update);
+});
+
+app.delete('/produtos/:idProduto', (req, res) => {
+    let idProduto = req.params.idProduto;
+    let posicao = 0;
+
+    for(let i of dados) {
+        if(i.id == idProduto) 
+            break;
+        posicao++;
+    }
+    dados.splice(posicao, 1);
+    res.status(204).send();
+});
+
 app.listen(8081, () => {
     console.log('Aplicação em execução na PORTA 8081');
 });
