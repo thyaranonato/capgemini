22ae7fa23848619c601de2e3c748f8a587b27fa1
Thyara Nonato <thyaranonato@outlook.com>
2022-05-02 11:56:05 -0300

    add crud cliente

---
+++ aula02-05/db.js
@@ -0,0 +1,16 @@
+let clientes = [
+    {
+    "id": 1,
+    "nome": "Maria",
+    "fone": "9999-9999",
+    "limiteCredito": 1000
+    },
+    {
+    "id": 2,
+    "nome": "Pedro",
+    "fone": "9999-9999",
+    "limiteCredito": 1000
+    }
+]
+
+module.exports = clientes;
--- aula02-05/index.js
+++ aula02-05/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const req = require('express/lib/request');
 const app = express();
+const dados = require('./db');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -49,6 +50,60 @@ app.put('/produtos/:idProduto', (req, res) => {
         preco: req.body.preco,
         estoque: req.body.estoque
     });
+});
+
+app.get('/clientes', (_req, res) => {
+    res.send(dados);
+});
+
+app.get('/clientes/:idCliente', (req, res) => {
+    let idCliente = req.params.idCliente;
+    let clienteRetornar = {};
+
+    for(let i of dados) {
+        if (i.id == idCliente) {
+            clienteRetornar = i;
+        }
+    }
+    res.status(200).send(clienteRetornar);
+});
+
+app.post('/clientes', (req, res) => {
+    let obj = {
+        id: req.body.id,
+        nome: req.body.nome,
+        fone: req.body.fone,
+        limiteCredito: req.body.limiteCredito
+    }
+    dados.push(obj);
+    res.status(201).send(obj);
+ });
+
+app.put('/clientes/:idCliente', (req, res) => {
+    let idCliente = req.params.idCliente;
+    let updateCliente = {};
+    for(let i of dados) {
+        if(i.id == idCliente) {
+            i.nome = req.body.nome,
+            i.fone = req.body.fone,
+            i.limiteCredito = req.body.limiteCredito
+            updateCliente = i;
+        }
+    }
+    res.status(200).send(updateCliente);
+});
+
+app.delete('/clientes/:idCliente', (req, res) => {
+    let idCliente = req.params.idCliente;
+    let posicao = 0;
+
+    for(let i of dados) {
+        if(i.id == idCliente)
+            break;
+        posicao++;
+    }
+    dados.splice(posicao, 1);
+    res.status(204).send();
 });
 
 app.listen(8081, () => {
