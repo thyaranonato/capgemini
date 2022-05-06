46d20a9091d6d29cad17c418faa7230813e10e93
Thyara Nonato <thyaranonato@outlook.com>
2022-05-06 14:24:56 -0300

    criando componentes

--- aula04-05/DB/app-login/src/app/app.module.ts
+++ aula04-05/DB/app-login/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    ConsultaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
