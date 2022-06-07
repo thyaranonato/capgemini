3d46c083420d2f2149cda3b150fa2b48d10ec314
Thyara Nonato <thyaranonato@outlook.com>
2022-06-07 16:50:06 -0300

    adiciona método de excluir a reserva

--- Java/MS-Locacao/src/main/java/com/aulas/mvc/controller/LocacaoController.java
+++ Java/MS-Locacao/src/main/java/com/aulas/mvc/controller/LocacaoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,13 @@ public class LocacaoController {
 	@PostMapping("/locacao")
 	public ResponseEntity<Locacao> save(@RequestBody Locacao locacao) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(locacao));
+		
+	}
+	
+	@DeleteMapping("/locacao/{id}")
+	public ResponseEntity<Void> cancelar(@PathVariable("id") Long id) {
+		service.cancelar(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		
 	}
 }
--- Java/MS-Locacao/src/main/java/com/aulas/mvc/service/LocacaoService.java
+++ Java/MS-Locacao/src/main/java/com/aulas/mvc/service/LocacaoService.java
@@ -1,6 +1,9 @@
 package com.aulas.mvc.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,5 +27,12 @@ public class LocacaoService {
 	public Locacao save(Locacao locacao) {
 		localFeignClient.reservar(locacao.getLocal(), true);
 		return repositorio.save(locacao);
+	}
+	
+	public void cancelar(Long id) {
+		Optional<Locacao> obj = repositorio.findById(id);
+		Locacao locacao = obj.orElseThrow(() -> new EntityNotFoundException("Locação não encontrada!"));
+		localFeignClient.reservar(locacao.getLocal(), false);
+		repositorio.delete(locacao);
 	}
 }
