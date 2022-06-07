32a9c15e0ddf5b7a079716d983c7b3db44925d4c
Thyara Nonato <thyaranonato@outlook.com>
2022-06-07 15:35:51 -0300

    criada comunicação entre microserviço locacao e local

--- Java/MS-Locacao/src/main/java/com/aulas/mvc/service/LocacaoService.java
+++ Java/MS-Locacao/src/main/java/com/aulas/mvc/service/LocacaoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aulas.mvc.entidades.Locacao;
+import com.aulas.mvc.feignclient.LocalFeignClient;
 import com.aulas.mvc.repositorio.LocacaoRepositorio;
 
 @Service
@@ -13,11 +14,15 @@ public class LocacaoService {
 	@Autowired
 	LocacaoRepositorio repositorio;
 	
+	@Autowired
+	LocalFeignClient localFeignClient;
+	
 	public List<Locacao> getAll() {
 		return repositorio.findAll();
 	}
 	
 	public Locacao save(Locacao locacao) {
+		localFeignClient.reservar(locacao.getLocal(), true);
 		return repositorio.save(locacao);
 	}
 }
